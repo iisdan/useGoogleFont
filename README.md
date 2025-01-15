fa8e5e54644d096171d114580d38a204ef2e676b
Dan <daniel@danielherbert.ca>
2025-01-15 18:24:50 -0500

    Use Google Font Hook

---
+++ README.md
@@ -0,0 +1,65 @@
+# useGoogleFont
+
+A lightweight React hook for dynamically loading and applying Google Fonts.
+
+## Installation
+
+```bash
+npm install use-google-font
+```
+
+# Usage
+
+## Basic Usage
+
+Simply call the hook with the font name you want to use and it will load the font in dynamically.
+
+
+```tsx
+import { useGoogleFont } from 'use-google-font';
+
+const App = () => {
+
+  useGoogleFont('Gigachad Gothic', { ...options })
+
+}
+```
+
+# Options
+
+## Weights
+
+Use the optional `weights` option to change which weights are loaded. 
+
+By default only normal 500 and 700 wieghts will be loaded. 
+
+```tsx
+{ 
+  weights: { 
+    normal: [500, 700], 
+    italic: [100] 
+  }
+}
+```
+
+## Default
+
+By setting `default` to `true`, the font will be set on the body tag automatically.
+
+```tsx
+{ 
+  default: true
+}
+```
+
+# Performance
+
+Optionally, you can add the following HTML snippet in your document’s head to optimize font loading. 
+Placing these <link> tags in the <head> ensures the preconnect requests happen early, reducing latency:
+
+```html
+<head>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin />
+</head>
+```
---
+++ src/useGoogleFont.ts
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+
+const fontWeights = [100,200,300,400,500,600,700,800,900] as const;
+type FontWeight = typeof fontWeights[number];
+
+const fallbackWeights: FontWeight[] = [400, 700];
+
+interface Options {
+  default?: boolean;
+  weights?: {
+    normal?: FontWeight[];
+    italic?: FontWeight[];
+  };
+}
+
+export const useGoogleFont = (name: string, options?: Options): void => {
+
+  function isBrowser() {
+    return typeof window !== 'undefined';
+  }
+
+  useEffect(() => {
+    if (!isBrowser()) {
+      return;
+    }
+
+    const normalWeights = options?.weights?.normal || fallbackWeights;
+    const italicWeights = options?.weights?.italic || [];
+
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+
+    const encodedName = encodeURIComponent(name);
+
+    const normalWeightString = normalWeights.join(';');
+    const italicWeightString = italicWeights.join(';');
+
+    link.href = `https://fonts.googleapis.com/css2?family=${encodedName}:wght@${normalWeightString};ital,wght@0,${normalWeightString};1,${italicWeightString}`;
+
+    if (options?.default) {
+      link.onload = () => {
+          document.body.style.fontFamily = name;
+      }
+    }
+
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, [name, options]);
+};
